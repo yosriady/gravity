1126a6638b97bb0ff3c62d08435910170e5a9ba5
Yos Riady <yosriady@gmail.com>
2016-09-06 15:07:32 +0800

    Implement Gravity.profile

--- README.md
+++ README.md
@@ -15,8 +15,20 @@ Add gravity to your list of dependencies in `mix.exs`:
 ## Usage
 
 ```
-  iex> Gravity.image("yosriady@gmail.com")
-  "https://www.gravatar.com/avatar/1b0ab2500efc1b91424fb49045312929"
+    iex> Gravity.image("yosriady@gmail.com")
+    "https://www.gravatar.com/avatar/1b0ab2500efc1b91424fb49045312929"
+
+    iex> Gravity.image("yosriady@gmail.com", size: 200, rating: :pg, default: :retro)
+    "https://www.gravatar.com/avatar/1b0ab2500efc1b91424fb49045312929?size=200&rating=pg&default=retro"
+
+    iex> Gravity.image("yosriady@gmail.com", s: 200, r: :pg, d: :retro)
+    "https://www.gravatar.com/avatar/1b0ab2500efc1b91424fb49045312929?s=200&r=pg&d=retro"
+
+    iex> Gravity.profile("yosriady@gmail.com")
+    "https://www.gravatar.com/1b0ab2500efc1b91424fb49045312929"
+
+    iex>Gravity.profile("yosriady@gmail.com", :qr)
+    "https://www.gravatar.com/1b0ab2500efc1b91424fb49045312929.qr"
 ```
 
 ## Configuration
--- lib/gravity.ex
+++ lib/gravity.ex
@@ -6,11 +6,17 @@ defmodule Gravity do
     iex> Gravity.image("yosriady@gmail.com")
     "https://www.gravatar.com/avatar/1b0ab2500efc1b91424fb49045312929"
 
-    iex> Gravity.profile("beau@automattic.com")
-    "https://www.gravatar.com/22bd03ace6f176bfe0c593650bcf45d8"
+    iex> Gravity.image("yosriady@gmail.com", size: 200, rating: :pg, default: :retro)
+    "https://www.gravatar.com/avatar/1b0ab2500efc1b91424fb49045312929?size=200&rating=pg&default=retro"
 
-    iex> Gravity.image("yosriady@gmail.com", s: 200, r: :g, d: :retro)
-    "https://www.gravatar.com/avatar/1b0ab2500efc1b91424fb49045312929?s=200&r=g&d=retro"
+    iex> Gravity.image("yosriady@gmail.com", s: 200, r: :pg, d: :retro)
+    "https://www.gravatar.com/avatar/1b0ab2500efc1b91424fb49045312929?s=200&r=pg&d=retro"
+
+    iex> Gravity.profile("yosriady@gmail.com")
+    "https://www.gravatar.com/1b0ab2500efc1b91424fb49045312929"
+
+    iex>Gravity.profile("yosriady@gmail.com", :qr)
+    "https://www.gravatar.com/1b0ab2500efc1b91424fb49045312929.qr"
 
   """
 
